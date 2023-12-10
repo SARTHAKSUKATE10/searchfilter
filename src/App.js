175a82e901c0e1e36b2968de87faa67a9352801f
jrsarthak10@gmail.com <jrsarthak10@gmail.com>
2023-12-10 11:16:09 +0530

    Style & functionality added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import JSONDATA from "./MOCK_DATA.json"
+import {useState} from 'react';
+
+
 function App() {
+const [searchTerm,setSearchTerm]=useState('')
+
   return (
     <div className="App">
-      <input type="text" placeholder="Search..." />
+      <input type="text" placeholder="Search..."
+      onChange={(event)=>{
+        setSearchTerm(event.target.value)
+      }}
+      />
 
-      {JSONDATA.map((value, key) => {
+      {JSONDATA.filter((value)=>{
+        if(searchTerm==''){
+          return value
+        }else if(value.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
+          return value
+        }
+      }).map((value, key) => {
         return (
           <div className="user" key={key}>
             <p>{value.first_name}</p>
